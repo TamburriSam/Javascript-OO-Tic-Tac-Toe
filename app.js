4a40d4d8af8bafd64af6f9700fddbbf2f544bbe8
tamburrisam <tamburrisam@gmail.com>
2021-02-27 15:19:26 -0500

    added wonGame logic

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let countbox = document.querySelector('.counter')
+
 const Gameboard = (function () {
   let one = document.querySelector("#boxone");
   let two = document.querySelector("#boxtwo");
@@ -16,20 +18,12 @@ const Gameboard = (function () {
 
 //showgame
 function showGame(gameboard) {
-  gameboard[0].textContent = "X";
-  gameboard[1].textContent = "X";
-  gameboard[2].textContent = "X";
-  gameboard[3].textContent = "X";
-  gameboard[4].textContent = "X";
-  gameboard[5].textContent = "X";
-  gameboard[6].textContent = "X";
-  gameboard[7].textContent = "X";
-  gameboard[8].textContent = "X";
+ let newGameArray = [];
+  for(let i = 0; i < Gameboard.length; i++){
+      newGameArray.push(Gameboard[i].textContent)
+  }
+  return newGameArray
 }
-
-
-
-//showGame(Gameboard);
 
 
 //Player
@@ -37,9 +31,6 @@ const Player = (token) => {
   const getToken = () => token;
   return getToken();
 }
-
-
-
 
 
 
@@ -54,69 +45,135 @@ const Game = (function () {
 
   const getTurn = (function(){
     if(count === 0){
-        Player1 = Player('X')
-        return Player1 + count++
+        Player1 = Player('X');
+        count++;
+        countbox.innerHTML = count;
+        return Player1 
 
     }else if(count === 1){
-        Player2 = Player('0')
-        return Player2 + ' ' + count++
+        Player2 = Player('O')
+        count++;
+        countbox.innerHTML = count;
+        return Player2
 
     }else if(count === 2){
         Player1 = Player('X')
-        return Player1 + ' ' + count++
+        count++;
+        countbox.innerHTML = count;
+        return Player1
 
     }else if(count === 3){
-        Player2 = Player('0')
-        return Player2 + ' ' + count++
+        Player2 = Player('O')
+        count++;
+        countbox.innerHTML = count;
+        return Player2
 
     }else if(count === 4){
         Player1 = Player('X')
-        return Player1 + ' ' + count++
+        count++;
+        countbox.innerHTML = count;
+        return Player1
 
     }else if(count === 5){
-        Player2 = Player('0')
-        return Player2 + ' ' + count++
+        Player2 = Player('O')
+        count++;
+        countbox.innerHTML = count;
+        return Player2
 
     }else if(count === 6){
         Player1 = Player('X')
-        return Player1 + ' ' + count++
+        count++;
+        countbox.innerHTML = count;
+        return Player1
 
     }else if(count === 7){
-        Player2 = Player('0')
-        return Player2 + ' ' + count++
+        Player2 = Player('O')
+        count++;
+        countbox.innerHTML = count;
+        return Player2
 
     }else if(count === 8){
         Player1 = Player('X')
-        return Player1 + ' ' + count++
+        count++;
+        countbox.innerHTML = count;
+        return Player1 
 
   }
 })
 
- const actualTurn = (function(){
 
-    if(count % 2 === 0){
-
-    }
- })
-
- const makeMove = (function(){
+const makeMove = (function(){
     for(let i = 0 ; i < Gameboard.length; i++){
         Gameboard[i].addEventListener('click', function(){
-            Gameboard[i].innerHTML = getTurn()
+            if(Gameboard[i].innerHTML === ""){
+            Gameboard[i].innerHTML += getTurn();
+            }
         })
     }
  })
 
 
-    const validMove = (function(){
+ const wonGame = (function(){
+     let gamePiece = showGame(Gameboard);
+//top row - 1
+if(gamePiece[0] && gamePiece[1] && gamePiece[2] === 'X'){
+    return console.log('X WON')}
 
-    })
+if(gamePiece[0] && gamePiece[1] && gamePiece[2] === 'O'){
+    return console.log('O WON')}
 
+//left side - 2
+if(gamePiece[0] && gamePiece[3] && gamePiece[6] === 'X'){
+    return console.log('X WON')}
 
+if(gamePiece[0] && gamePiece[3] && gamePiece[6] === 'O'){
+    return console.log('O WON')
+}
 
+ //bottom row - 3
+if(gamePiece[6] && gamePiece[7] && gamePiece[8] === 'X'){
+    return console.log('X WON')
+}else if(gamePiece[6] && gamePiece[7] && gamePiece[8] === 'O'){
+    return console.log('O WON')
+}
 
+//right side - 4
+if(gamePiece[2] && gamePiece[5] && gamePiece[8] === 'X'){
+    return console.log('X WON')
+}else if(gamePiece[2] && gamePiece[5] && gamePiece[8] === 'O'){
+    return console.log('O WON')
+}
 
-  return {getTurn, makeMove};
+//diagonal left top corner - 5
+if(gamePiece[0] && gamePiece[4] && gamePiece[8] === 'X'){
+    return console.log('X WON')
+}else if(gamePiece[0] && gamePiece[4] && gamePiece[8] === 'O'){
+    console.log('O WON')
+}
+
+//diagonal right top corner - 6
+if(gamePiece[2] && gamePiece[4] && gamePiece[6] === 'X'){
+    return console.log('X WON')
+}else if(gamePiece[2] && gamePiece[4] && gamePiece[6] === 'O'){
+    return console.log('O WON')
+} 
+
+//down the middle horizontal - 7
+if(gamePiece[3] && gamePiece[4] && gamePiece[5] === 'X'){
+    return console.log('X WON')
+}else if(gamePiece[3] && gamePiece[4] && gamePiece[5] === 'O'){
+    return console.log('O WON')
+}
+
+//down the middle vertical - 8
+if(gamePiece[1] && gamePiece[4] && gamePiece[7] === 'X'){
+    return console.log('X WON')
+}else if(gamePiece[1] && gamePiece[4] && gamePiece[7] === 'O'){
+    return console.log('O WON')
+}
+
+})
+
+  return {getTurn, makeMove, wonGame};
 })();
-
 
