71e6ca3de52bb807169c47c89c9a203edee94134
tamburrisam <tamburrisam@gmail.com>
2021-03-03 15:19:25 -0500

    added refreshGame function

--- app.js
+++ app.js
@@ -29,9 +29,11 @@ const Player = (token) => {
 
 //Game
 const Game = (function () {
-  let Player1 = Player("X");
-  let Player2 = Player("O");
+  let Player1 = '';
+  let Player2 = '';
   let count = 0;
+  var playerName1 = document.querySelector(".player1name");
+  var playerName2 = document.querySelector(".player2name");
 
   const _getTurn = function () {
     if (count % 2 === 0) {
@@ -45,11 +47,10 @@ const Game = (function () {
     }
   };
 
+
   const showName = function () {
     var lets = document.querySelector(".lets");
     var play = document.querySelector(".play");
-    var playerName1 = document.querySelector(".player1name");
-    var playerName2 = document.querySelector(".player2name");
     var ready1 = document.querySelector(".name1");
     var ready2 = document.querySelector(".name2");
     var player1card = document.querySelector(".playeronecard");
@@ -59,7 +60,7 @@ const Game = (function () {
     var vs = document.querySelector(".vs");
 
     ready1.addEventListener("click", function () {
-      lets.innerHTML = playerName1.value;
+      lets.innerHTML = playerName1.value+' ';
       lets.style.color = "#22A2F2";
       lets.appendChild(vs);
       vs.style.display = "inline";
@@ -68,11 +69,11 @@ const Game = (function () {
     });
 
     ready2.addEventListener("click", function () {
-      play.innerHTML = playerName2.value;
+      play.innerHTML = '&nbsp' + playerName2.value;
       play.style.color = "orange";
       player2card.style.display = "none";
       tictactoe.style.position = "relative";
-      tictactoe.style.bottom = "250px";
+      tictactoe.style.bottom = "230px";
       tictactoe.style.display = "block";
       tictactoe.style.fontSize = "12px";
       tictactoe.style.position = "absolute";
@@ -109,8 +110,9 @@ const Game = (function () {
     var play = document.querySelector(".play");
     let winnerBox = document.querySelector(".winnerbox");
     let winner = document.querySelector(".box");
+    let playAgain = document.querySelector('.yes')
 
-    lets.style.display = "none";
+    lets.innerHTML=''
     winnerBox.style.display = "block";
 
     if(winner.textContent != 'X'){
@@ -119,149 +121,27 @@ const Game = (function () {
       play.innerHTML = 'X wins'
     }
 
-    //play.innerHTML = `${winner} wins!`;
 
-    return true //(play.innerHTML = `${winner.textContent} wins!`);
+    return _refreshGame(); 
   };
 
- /*   const winCombo = function () {
-    //top row - 1
-    if (
-      Gameboard[0].textContent === "X" &&
-      Gameboard[1].textContent === "X" &&
-      Gameboard[2].textContent === "X"
-    ) {
-      return _renderWinner();
-    }
+  function _refreshGame(){
+    let playAgain = document.querySelector('.yes')
+    let winnerBox = document.querySelector(".winnerbox");
+    var lets = document.querySelector(".lets");
+    var play = document.querySelector(".play");
 
-    if (
-      Gameboard[0].textContent === "O" &&
-      Gameboard[1].textContent === "O" &&
-      Gameboard[2].textContent === "O"
-    ) {
-      return _renderWinner();
-    }
+    playAgain.addEventListener('click',function(){
+      winnerBox.style.display="none"
+      for(i = 0 ; i < _showGame(Gameboard).length; i++){
+        Gameboard[i].innerHTML = '';
+        lets.innerHTML = playerName1.value + ' vs ';
+        play.innerHTML = ' ' + playerName2.value;
+      }
+    })
+  }
 
-    //left side
-    if (
-      Gameboard[0].textContent === "X" &&
-      Gameboard[3].textContent === "X" &&
-      Gameboard[6].textContent === "X"
-    ) {
-      return _renderWinner();
-    }
-
-    if (
-      Gameboard[0].textContent === "O" &&
-      Gameboard[3].textContent === "O" &&
-      Gameboard[6].textContent === "O"
-    ) {
-      return _renderWinner();
-    }
-
-    //right side
-    if (
-      Gameboard[2].textContent === "X" &&
-      Gameboard[5].textContent === "X" &&
-      Gameboard[8].textContent === "X"
-    ) {
-      return _renderWinner();
-    }
-
-    if (
-      Gameboard[2].textContent === "O" &&
-      Gameboard[5].textContent === "O" &&
-      Gameboard[8].textContent === "O"
-    ) {
-      return _renderWinner();
-    }
-
-    //bottom
-    if (
-      Gameboard[6].textContent === "X" &&
-      Gameboard[7].textContent === "X" &&
-      Gameboard[8].textContent === "X"
-    ) {
-      return _renderWinner();
-    }
-
-    if (
-      Gameboard[6].textContent === "O" &&
-      Gameboard[7].textContent === "O" &&
-      Gameboard[8].textContent === "O"
-    ) {
-      return _renderWinner();
-    }
-
-    //horizontal middle
-    if (
-      Gameboard[3].textContent === "X" &&
-      Gameboard[4].textContent === "X" &&
-      Gameboard[5].textContent === "X"
-    ) {
-      return _renderWinner();
-    }
-
-    if (
-      Gameboard[3].textContent === "O" &&
-      Gameboard[4].textContent === "O" &&
-      Gameboard[5].textContent === "O"
-    ) {
-      return _renderWinner();
-    }
-
-    //vertical middle
-    if (
-      Gameboard[1].textContent === "X" &&
-      Gameboard[4].textContent === "X" &&
-      Gameboard[7].textContent === "X"
-    ) {
-      return _renderWinner();
-    }
-    if (
-      Gameboard[1].textContent === "O" &&
-      Gameboard[4].textContent === "O" &&
-      Gameboard[7].textContent === "O"
-    ) {
-      return _renderWinner();
-    }
-
-    //diagonal top left
-    if (
-      Gameboard[2].textContent === "X" &&
-      Gameboard[4].textContent === "X" &&
-      Gameboard[6].textContent === "X"
-    ) {
-      return _renderWinner();
-    }
-
-    if (
-      Gameboard[2].textContent === "O" &&
-      Gameboard[4].textContent === "O" &&
-      Gameboard[6].textContent === "O"
-    ) {
-      return _renderWinner();
-    }
-
-    //diagonal top right
-    if (
-      Gameboard[0].textContent === "X" &&
-      Gameboard[4].textContent === "X" &&
-      Gameboard[8].textContent === "X"
-    ) {
-      return _renderWinner();
-    }
-
-    if (
-      Gameboard[0].textContent === "O" &&
-      Gameboard[4].textContent === "O" &&
-      Gameboard[8].textContent === "O"
-    ) {
-      return _renderWinner();
-    }
-
-    return true;
-  };  */
+ 
   /*maybe return the persons player token lit up or something but something to return or modal */
   //look into returning true or false
   //god u gotta look at returns in general youre not v good w that yet
@@ -288,7 +168,7 @@ const Game = (function () {
 
       if(gamepiece[0] === 'O' && gamepiece[1] === 'O' && gamepiece[2] === 'O'
       || gamepiece[3] === 'O' && gamepiece[4] === 'O' && gamepiece[5] === 'O'
-      || gamepiece[6] === 'O' && gamepiece[7] === 'O' && gamepiece[8] === 'X'
+      || gamepiece[6] === 'O' && gamepiece[7] === 'O' && gamepiece[8] === 'O'
       || gamepiece[0] === 'O' && gamepiece[3] === 'O' && gamepiece[6] === 'O'
       || gamepiece[1] === 'O' && gamepiece[4] === 'O' && gamepiece[7] === 'O'
       || gamepiece[2] === 'O' && gamepiece[5] === 'O' && gamepiece[8] === 'O'
