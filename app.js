7080fbcc92713317b656920e01c599a89938b3f5
tamburrisam <tamburrisam@gmail.com>
2021-03-06 12:51:46 -0500

    media query

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ const Player = (token) => {
 
 //Game
 const Game = (function () {
-  let Player1 = '';
-  let Player2 = '';
+  let Player1 = "";
+  let Player2 = "";
   let count = 0;
   var playerName1 = document.querySelector(".player1name");
   var playerName2 = document.querySelector(".player2name");
@@ -63,64 +63,48 @@ const Game = (function () {
     var TicTacToe = document.querySelector(".othercontainer");
     var tttGameBoard = document.querySelector(".container");
     var vs = document.querySelector(".vs");
-    let popUp = document.querySelector('.popup')
-
-
- 
+    let popUp = document.querySelector(".popup");
 
     ready1.addEventListener("click", function () {
-   
-    
-      if(playerName1.value !== ''){
-      lets.innerHTML = playerName1.value+' ';
-      lets.style.color = "#22A2F2";
-      lets.appendChild(vs);
-      vs.style.display = "inline";
-      play.innerHTML = "";
-      player1card.style.display = "none";
-      ready2.disabled = false
-      }else {
+      if (playerName1.value !== "") {
+        lets.innerHTML = playerName1.value + " ";
+        lets.style.color = "#22A2F2";
+        lets.appendChild(vs);
+        vs.style.display = "inline";
+        play.innerHTML = "";
+        player1card.style.display = "none";
+        ready2.disabled = false;
+      } else {
+        popUp.style.display = "block";
+        player1card.appendChild(popUp);
 
-          popUp.style.display="block"
-          player1card.appendChild(popUp)
+        setTimeout(function () {
+          popUp.style.display = "none";
+        }, 2000);
 
-         
-         setTimeout(function(){
-           popUp.style.display="none"
-         }, 2000)
-      
-
-        player1card.style.display="block";
+        player1card.style.display = "block";
         lets.style.display = "block";
-        lets.innerHTML = `Lets`+`&nbsp`
-        play.style.display="block";
-        play.innerHTML="Play"
+        lets.innerHTML = `Lets` + `&nbsp`;
+        play.style.display = "block";
+        play.innerHTML = "Play";
       }
     });
 
-    
-
     ready2.addEventListener("click", function () {
+      if (playerName2.value !== "") {
+        play.innerHTML = "&nbsp" + playerName2.value;
+        play.style.color = "orange";
+        player2card.style.display = "none";
+        TicTacToe.style.gridColumn = "1";
+        tttGameBoard.style.display = "grid";
+      } else {
+        popUp.style.display = "block";
+        player2card.appendChild(popUp);
 
-      if(playerName2.value !== ''){
-      play.innerHTML = '&nbsp' + playerName2.value;
-      play.style.color = "orange";
-      player2card.style.display = "none";
-      TicTacToe.style.gridColumn = '1';
-      tttGameBoard.style.display = "grid";
-      }else{
-        popUp.style.display="block"
-          player2card.appendChild(popUp)
-
-         
-         setTimeout(function(){
-           popUp.style.display="none"
-         }, 2000)
+        setTimeout(function () {
+          popUp.style.display = "none";
+        }, 2000);
       }
-
-
-
-
     });
   };
 
@@ -153,65 +137,68 @@ const Game = (function () {
     var play = document.querySelector(".play");
     let winnerBox = document.querySelector(".winnerbox");
 
-    lets.innerHTML=''
+    lets.innerHTML = "";
     winnerBox.style.display = "flex";
-    play.innerHTML = `${won[won.length-1]} wins!`
-    console.log(won)
+    play.innerHTML = `${won[won.length - 1]} wins!`;
+    console.log(won);
 
-    return _refreshGame(); 
+    return _refreshGame();
   };
 
-  function _refreshGame(){
-    let playAgain = document.querySelector('.yes')
+  function _refreshGame() {
+    let playAgain = document.querySelector(".yes");
     let winnerBox = document.querySelector(".winnerbox");
     var lets = document.querySelector(".lets");
     var play = document.querySelector(".play");
 
-    playAgain.addEventListener('click',function(){
-      winnerBox.style.display="none"
-      for(i = 0 ; i < _showGame(GameBoard).length; i++){
-        GameBoard[i].innerHTML = '';
+    playAgain.addEventListener("click", function () {
+      winnerBox.style.display = "none";
+      for (i = 0; i < _showGame(GameBoard).length; i++) {
+        GameBoard[i].innerHTML = "";
         //huhhhhhhhhhhhhh
-        lets.innerHTML = playerName1.value + '&nbspvs&nbsp ';
-        play.innerHTML = ' ' + playerName2.value;
+        lets.innerHTML = playerName1.value + "&nbspvs&nbsp ";
+        play.innerHTML = " " + playerName2.value;
       }
-    })
+    });
   }
 
+  const winCombo = function () {
+    let gamePiece = _showGame(GameBoard);
 
-   const winCombo = function () {
-    let gamePiece = _showGame(GameBoard)
+    if (
+      (gamePiece[0] === "X" && gamePiece[1] === "X" && gamePiece[2] === "X") ||
+      (gamePiece[3] === "X" && gamePiece[4] === "X" && gamePiece[5] === "X") ||
+      (gamePiece[6] === "X" && gamePiece[7] === "X" && gamePiece[8] === "X") ||
+      (gamePiece[0] === "X" && gamePiece[3] === "X" && gamePiece[6] === "X") ||
+      (gamePiece[1] === "X" && gamePiece[4] === "X" && gamePiece[7] === "X") ||
+      (gamePiece[2] === "X" && gamePiece[5] === "X" && gamePiece[8] === "X") ||
+      (gamePiece[0] === "X" && gamePiece[4] === "X" && gamePiece[8] === "X") ||
+      (gamePiece[2] === "X" && gamePiece[4] === "X" && gamePiece[6] === "X")
+    ) {
+      won.push("X");
+      _renderWinner();
 
+      return true;
+    }
 
-    if(gamePiece[0] === 'X' && gamePiece[1] === 'X' && gamePiece[2] === 'X'
-      || gamePiece[3] === 'X' && gamePiece[4] === 'X' && gamePiece[5] === 'X'
-      || gamePiece[6] === 'X' && gamePiece[7] === 'X' && gamePiece[8] === 'X'
-      || gamePiece[0] === 'X' && gamePiece[3] === 'X' && gamePiece[6] === 'X'
-      || gamePiece[1] === 'X' && gamePiece[4] === 'X' && gamePiece[7] === 'X'
-      || gamePiece[2] === 'X' && gamePiece[5] === 'X' && gamePiece[8] === 'X'
-      || gamePiece[0] === 'X' && gamePiece[4] === 'X' && gamePiece[8] === 'X'
-      || gamePiece[2] === 'X' && gamePiece[4] === 'X' && gamePiece[6] === 'X'){
-        won.push('X')
-        _renderWinner()
-        
-      return true
-      }
+    if (
+      (gamePiece[0] === "O" && gamePiece[1] === "O" && gamePiece[2] === "O") ||
+      (gamePiece[3] === "O" && gamePiece[4] === "O" && gamePiece[5] === "O") ||
+      (gamePiece[6] === "O" && gamePiece[7] === "O" && gamePiece[8] === "O") ||
+      (gamePiece[0] === "O" && gamePiece[3] === "O" && gamePiece[6] === "O") ||
+      (gamePiece[1] === "O" && gamePiece[4] === "O" && gamePiece[7] === "O") ||
+      (gamePiece[2] === "O" && gamePiece[5] === "O" && gamePiece[8] === "O") ||
+      (gamePiece[0] === "O" && gamePiece[4] === "O" && gamePiece[8] === "O") ||
+      (gamePiece[2] === "O" && gamePiece[4] === "O" && gamePiece[6] === "O")
+    ) {
+      won.push("O");
+      _renderWinner();
 
-      if(gamePiece[0] === 'O' && gamePiece[1] === 'O' && gamePiece[2] === 'O'
-      || gamePiece[3] === 'O' && gamePiece[4] === 'O' && gamePiece[5] === 'O'
-      || gamePiece[6] === 'O' && gamePiece[7] === 'O' && gamePiece[8] === 'O'
-      || gamePiece[0] === 'O' && gamePiece[3] === 'O' && gamePiece[6] === 'O'
-      || gamePiece[1] === 'O' && gamePiece[4] === 'O' && gamePiece[7] === 'O'
-      || gamePiece[2] === 'O' && gamePiece[5] === 'O' && gamePiece[8] === 'O'
-      || gamePiece[0] === 'O' && gamePiece[4] === 'O' && gamePiece[8] === 'O'
-      || gamePiece[2] === 'O' && gamePiece[4] === 'O' && gamePiece[6] === 'O'){
-        won.push('O')
-        _renderWinner()
-       
-        return true
-      }else{
-      return false}
-};  
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   const fullGame = function () {
     showName(), makeMove(), winCombo();
